d21f8f5cc5dba68522c2adc0a30f15afa407cb40
Danny <danny.hj.vogel@gmail.com>
2024-06-17 00:34:01 +0200

    WIP: route to create new gameList using IGDB IDs and logging games not found

--- server/routes/gameListIds.post.ts
+++ server/routes/gameListIds.post.ts
@@ -1,18 +1,17 @@
 export default eventHandler(async (event) => {
-  const gameStorage = useStorage("gameList");
-  const errorGames = {};
   const config = useRuntimeConfig();
   const IGDBClientId = config.IGDBClientId;
   const token = getRequestHeader(event, "Authorization");
   const body = await readBody(event);
-  const gameList = body.gameList as Game[];
+  const oldGameList = body.gameList as Games;
+  const newGameList = {} as Games;
+  const errorGames = {};
 
-  for (const game of gameList) {
-    const gameTitle = game.name;
-    const gameReleaseDate = game.released;
+  for (const id in oldGameList) {
+    const gameTitle = oldGameList[id].name;
+    const gameReleaseDate = oldGameList[id].released;
     let date;
     if (gameReleaseDate) date = new Date(gameReleaseDate).getTime();
-    console.log("getting:", gameTitle);
 
     const response = await $fetch("https://api.igdb.com/v4/games", {
       method: "POST",
@@ -25,6 +24,7 @@ export default eventHandler(async (event) => {
         date ? `& first_release_date = ${date}` : ""
       };`,
     });
+    console.log("response", response);
     if (!response) {
       throw createError({ statusCode: 500, message: response.message });
     } else if (Array.isArray(response)) {
@@ -37,15 +37,27 @@ export default eventHandler(async (event) => {
         errorGames[gameTitle] = response;
         continue;
       } else {
-        game.twitchId = response[0].id;
-        game.twitchName = response[0].name;
+        newGameList[response[0].id] = oldGameList[id];
+        newGameList[response[0].id].id = response[0].id;
       }
     }
   }
-  console.log("errorGames", errorGames);
-  gameStorage.setItem("errorGames", JSON.stringify(errorGames));
+  $fetch("/log/errors/gameList", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newGameList),
+  });
+  $fetch("/log/errors/errorGames", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(errorGames),
+  });
   return {
     statusCode: 200,
-    body: JSON.stringify(gameList),
+    status: "success",
   };
 });
